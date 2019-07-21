7a5357b8c0d32e88b5c4d220024e1ef22ddc4eae
Ivan <Kishkan91@gmail.com>
2019-07-21 18:15:37 +0400

    Initial commit

---
+++ src/MainReader.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Andrey_Vaganov on 12/5/2016.
+ */
+public class MainReader {
+
+    /**
+     * Формат даты
+     */
+    private static final String DATE_FORMAT_PATTERN = "dd.MM.yyyy";
+
+    /**
+     * Форматтер, используется для преобразования строк в даты и обратно
+     */
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat(DATE_FORMAT_PATTERN);
+
+
+    /**
+     * Точка входа в программу
+     * @param args
+     */
+    public static void main(String[] args) {
+        readFile();
+    }
+
+    /**
+     * Метод для чтения дат из файла
+     */
+    public static void readFile() {
+        //Открываем потоки на чтение из файла
+        try {
+            FileReader reader = new FileReader("file.txt");
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+
+
+        BufferedReader byfReader = new BufferedReader(reader);
+
+        //Читаем первую строку из файла
+        String strDate = byfReader.readLine();
+
+        while(strDate != null) {
+            //Преобразуем строку в дату
+            Date date = parseDate(strDate);
+
+            //Выводим дату в консоль в формате dd-mm-yy
+            System.out.printf("%1$td-%1$tm-%1$ty \n", date);
+
+            //Читаем следующую строку из файла
+            strDate = byfReader.readLine();
+
+        }
+    }
+
+    /**
+     * Метод преобразует строковое представление даты в класс Date
+     * @param strDate строковое представление даты
+     * @return
+     */
+    public static Date parseDate(String strDate) {
+        return dateFormatter.parse(strDate);
+    }
+}
